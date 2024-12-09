f4a59532c2d4a1ef74e50a5d21744644709efca6
Evolex Inc. <100163406+evolexinc@users.noreply.github.com>
2024-12-10 03:39:23 +0530

    Update port_scanner.py

--- port_scanner.py
+++ port_scanner.py
@@ -1,1 +1,15 @@
+''' Use It for Education Purpose only 
+copyright 2024 Evolex Inc. '''
+import socket
+
+def scan_port(ip, port):
+	sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	sock.settimeout(1)
+	result = sock.connect_ex((ip, port))
+	sock.close()
+	return result == 0
+target_ip =input("Enter IP Address:")
+for port in range(1, 1025):
+	if scan_port(target_ip, port):
+		print(f"https://{target_ip}:{port} is open")
 
